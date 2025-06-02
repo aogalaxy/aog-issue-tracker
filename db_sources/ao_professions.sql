3355a2a09f72354d376bbd09d074423631d2f429
aogalaxy <aog@aogalaxy.com>
2025-06-02 10:31:51 -1000

    Add files via upload

---
+++ db_sources/ao_professions.sql
@@ -0,0 +1,14 @@
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (1, 9, 'Enforcer', 117926);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (2, 12, 'Meta-Physicist', 16308);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (3, 6, 'Adventurer', 84203);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (4, 14, 'Keeper', 38911);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (5, 15, 'Shade', 39290);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (6, 1, 'Soldier', 301604);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (7, 11, 'Nano-Technician', 45190);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (8, 7, 'Trader', 117993);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (9, 5, 'Agent', 16186);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (10, 4, 'Fixer', 16300);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (11, 3, 'Engineer', 287091);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (12, 2, 'Martial Artist', 301439);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (13, 8, 'Bureaucrat', 293898);
+INSERT INTO `ao_professions` (`aoProfID`, `aoDBProfID`, `aoProf`, `aoIconID`) VALUES (14, 10, 'Doctor', 44235);
---
+++ db_sources/skill_combine_master.sql
@@ -0,0 +1,8 @@
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (1, 'nano', 'Nano Skill');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (2, 'evades', 'Evades');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (3, 'addDMG', 'Add Damage');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (4, 'ac', 'AC');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (5, 'inits', 'Inits');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (6, 'dmgShield', 'Shield');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (7, 'dmgAbsorb', 'Absorb');
+INSERT INTO `skill_combine_master` (`skillCombineID`, `combineType`, `combinedName`) VALUES (8, 'dmgReflect', 'Reflect');
